94e1c23d6361dda5f6651e28c9ee234fc3ef4f2f
willlldan <173040071@mail.unpas.ac.id>
2022-02-27 07:05:47 +0700

    Video 11 - Contacts.app (Part 2)

--- contact.app/app.js
+++ contact.app/app.js
@@ -1,11 +1,43 @@
-const { ask, save } = require("./contacts");
+// Mengambil argument dari CLI
 
-const main = async () => {
-  const name = await ask("Masukan nama anda : ");
-  const email = await ask("Masukan email anda : ");
-  const noHp = await ask("Masukan no hp anda : ");
+const yargs = require("yargs");
+const contacts = require("./contacts");
 
-  save(name, email, noHp);
-};
+yargs.command({
+  command: "add",
+  describe: "Add new contact",
+  builder: {
+    nama: {
+      describe: "Nama Lengkap",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "Email",
+      demandOption: false,
+      type: "string",
+    },
+    noHp: {
+      describe: "No Hp",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    contacts.save(argv.nama, argv.email, argv.noHp);
+  },
+});
 
-main();
+yargs.parse();
+
+// const { ask, save } = require("./contacts");
+
+// const main = async () => {
+//   const name = await ask("Masukan nama anda : ");
+//   const email = await ask("Masukan email anda : ");
+//   const noHp = await ask("Masukan no hp anda : ");
+
+//   save(name, email, noHp);
+// };
+
+// main();
--- contact.app/contacts.js
+++ contact.app/contacts.js
@@ -1,41 +1,51 @@
 const fs = require("fs");
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const chalk = require("chalk");
+const validator = require("validator");
 
 // check folder
 const dirPath = "./data";
 if (!fs.existsSync(dirPath)) {
+  console.log("kesini juga");
   fs.mkdirSync(dirPath);
 }
 
 // check file
 const dataPath = "./data/contacts.json";
-if (!fs.existsSync()) {
+if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
-
-const ask = (question) => {
-  return new Promise((resolve, rejects) => {
-    rl.question(question, (answer) => {
-      resolve(answer);
-    });
-  });
-};
 
 const save = (name, email, noHp) => {
   const contact = { name, email, noHp };
   const file = fs.readFileSync("data/contacts.json", "utf-8");
   const contacts = JSON.parse(file);
 
+  // Cek duplikasi
+  const duplicate = contacts.find((contact) => contact.name === name);
+  if (duplicate) {
+    console.log(chalk.red.inverse.bold("Contact sudah terdaftar"));
+    return false;
+  }
+
+  // Cek email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.inverse.bold("Email tidak valid"));
+      return false;
+    }
+  }
+
+  // cek no hp
+  if (!validator.isMobilePhone(noHp, "id-ID")) {
+    console.log(chalk.red.inverse.bold("No Hp tidak valid"));
+    return false;
+  }
+
   contacts.push(contact);
 
   fs.writeFileSync("data/contacts.json", JSON.stringify(contacts, null, 2));
 
-  rl.close();
+  console.log(chalk.green.inverse.bold("Terimakasih telah menginputkan data"));
 };
 
-module.exports = { ask, save };
+module.exports = { save };
